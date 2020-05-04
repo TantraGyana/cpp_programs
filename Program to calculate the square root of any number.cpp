cd0cd51a9d82115c1c65394bdb36fd683561b59f
TantraGyana <64749331+TantraGyana@users.noreply.github.com>
2020-05-04 14:21:31 +0530

    Create Program to calculate the square root of any number.cpp

---
+++ Program to calculate the square root of any number.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include < math.h > 
+
+using namespace std;  
+
+int main() 
+{  
+    double n;  
+    cout<<"Enter the Number of which you want the Square Root: ";
+    cin >> n;  
+    double res;  
+    res = sqrt(n);  
+    cout << "\n" << "Square Root of " << n << " is: " << res;  
+    return 0;  
+}
