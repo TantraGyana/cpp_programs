aa07fd41bd33ad7442c474aa1302c0c6d3e1563f
TantraGyana <64749331+TantraGyana@users.noreply.github.com>
2020-05-04 14:12:08 +0530

    Create Program for calculating tangent of an angle.cpp

---
+++ Program for calculating tangent of an angle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>  
+#include <math.h>
+
+#define PI 3.14159265;  
+
+using namespace std; 
+
+int main() 
+{  
+    float angle, ans;  
+    cout << "Enter Value of Angle (in degrees): ";  
+    cin >> angle;  
+    ans = tan(angle * PI / 180);  
+    cout<<"\n"<<"tan "<<angle<<" = "<<ans;  
+    cout<<"\n Tangent of "<<angle<<" degree is: "<<ans;
+    return 0;  
+}  
