90d772ea1d80d2fbcc936bc1bdc8b83a6b6ca777
TantraGyana <64749331+TantraGyana@users.noreply.github.com>
2020-05-04 14:19:54 +0530

    Create Program to calculate square of any number.cpp

---
+++ Program to calculate square of any number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;  
+
+int main() 
+{  
+    double n, ans;  
+    cout << "Enter the Number of which you want the Square: ";  
+    cin >> n;  
+    ans = n * n;  
+    cout << "\n" << n << "^2" << " = " << ans;  
+    cout << "\n" << "Square of " << n << " is: " << ans;  
+    return 0;  
+} 
