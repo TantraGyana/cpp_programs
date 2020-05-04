e2ae4d8ca02f741dce8953f41888d3c59d05d083
TantraGyana <64749331+TantraGyana@users.noreply.github.com>
2020-05-04 15:58:41 +0530

    Create Pattern Printing (40).cpp

---
+++ Pattern Printing (40).cpp
@@ -0,0 +1,69 @@
+/*
+
+       *
+      * *
+     *   *
+    *     *
+   *       *
+  *         *
+ *           *
+*             *
+ *           *
+  *         *
+   *       *
+    *     *
+     *   *
+      * *
+       *
+
+*/
+
+#include <iostream>  
+  
+using namespace std;  
+  
+int main()  
+{  
+    int i, j, n, space_pyramid;  
+    cout << "Enter No. of Rows: ";  
+    cin >> n;  
+    for(i = 1; i <= n; i++)  
+    {  
+        for (space_pyramid = i; space_pyramid < n; space_pyramid++)  
+        {  
+            cout << " ";  
+        }  
+        for(j = 1; j <= (2 * i - 1); j++)  
+        {  
+            if(j == 1 || j == 2*i - 1)  
+            {  
+                cout << "*";  
+            }  
+            else  
+            {  
+                cout << " ";  
+            }  
+        }  
+        cout << "\n";  
+    }  
+    for(i = n-1; i >= 1; i--)  
+    {  
+        for (space_pyramid = i; space_pyramid < n; space_pyramid++)  
+        {  
+            cout << " ";  
+        }  
+        for(j = 1; j <= (2 * i - 1); j++)  
+        {  
+            if(i == n || j == 1 || j == 2*i - 1)  
+            {  
+                cout << "*";  
+            }  
+            else  
+            {  
+                cout << " ";  
+            }  
+        }  
+        cout << "\n";  
+    }  
+    return 0;  
+} 
