ae69226bb0228caba208744ecf3231cf433cf621
TantraGyana <64749331+TantraGyana@users.noreply.github.com>
2020-05-04 15:38:41 +0530

    Create Pattern Printing (35).cpp

---
+++ Pattern Printing (35).cpp
@@ -0,0 +1,44 @@
+/*
+
+*
+* *
+* * *
+* * * * 
+* * * * *
+* * * * * *
+* * * * * * *
+* * * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+#include <iostream>  
+using namespace std;  
+  
+int main()  
+{  
+    int i, j, n;  
+   cout << "Enter No. of Rows: ";  
+   cin >> n;  
+   for(i = 1; i <= n; i++)  
+   {  
+      for(j = 1; j <= i; j++)  
+      {  
+         cout << "* ";  
+      }  
+      cout << "\n";  
+   }  
+   for(i = n-1; i >= 1; i--)  
+   {  
+      for(j = 1; j <= i; j++)  
+      {  
+         cout << "* ";  
+      }  
+      cout << "\n";  
+   }  
+   return 0;  
+}  
