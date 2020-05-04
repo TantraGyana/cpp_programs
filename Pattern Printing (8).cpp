c6ecf332914b735c4479a865c7e50f7c25f352bd
TantraGyana <64749331+TantraGyana@users.noreply.github.com>
2020-05-04 14:38:15 +0530

    Create Pattern Printing (8).cpp

---
+++ Pattern Printing (8).cpp
@@ -0,0 +1,27 @@
+/*
+
+1  2  3  4  5
+1  2  3  4
+1  2  3
+1  2
+1
+
+*/
+
+#include <iostream>  
+  
+using namespace std;  
+  
+int main()  
+{  
+    int i, j;  
+    for(i = 5; i > 0; i--)  
+    {  
+        for(j = 1; j <= i ; j++)  
+        {  
+            cout << j << " ";  
+        }  
+        cout << "\n";  
+    }  
+    return 0;  
+}
